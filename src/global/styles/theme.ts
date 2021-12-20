01abb50560b940ed91a5d85d4be91b20a8dbb982
Lazaro1 <lazarolimasilva2@gmail.com>
2021-12-20 20:26:50 -0300

    finish view login

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+    colors: {
+        background: '#0d133d',
+        heading: '#DDE3f0',
+        primary: '#E61C44',
+        line: '#991F36'
+    }
+}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,21 +1,41 @@
 import { useState } from 'react'
-import { View, Text, TextInput  } from 'react-native'
+import { 
+    View, 
+    Text, 
+    Image,
+    StatusBar
+} from 'react-native'
+import { ButtonIcon } from '../../components/ButtonIcon'
+import IlustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
 
 export function SignIn(){
-    const [text, setText] = useState('')
     return(
         <View style={styles.container}>
-        <Text> Hello Word</Text>
+            <StatusBar 
+                barStyle='light-content' 
+                backgroundColor='transparent'
+                translucent
+            />
+            <Image 
+                source={IlustrationImg} 
+                style={styles.image}
+                resizeMode="stretch"
+            />
 
-        <TextInput 
-            style={styles.Input}  
-            onChangeText={setText}
-        />
-
-        <Text>
-            Voce Digitou {text}
-        </Text>
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Conecte-se{`\n`}
+                    e organize suas{`\n`} 
+                    jogatinas
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games{`\n`} 
+                    favoritos com seus amigos{`\n`} 
+                </Text>
+                <ButtonIcon title={'Discord'} activeOpacity={0.7}/>
+            </View>
+        
         </View>
     )
 }
