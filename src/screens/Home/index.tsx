c4d50a9e3445ee62555cdbe44f9aa7c1a04171b3
Alvaro Ferreira <alvarojfjunior@gmail.com>
2021-12-23 21:24:05 -0300

    tst mac

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import { Text, View } from 'react-native'
 import { ButtonAdd } from '../../components/ButtonAdd'
 import { CategorySelect } from '../../components/CategorySelect'
-
+//teste
 import { Profile } from '../Profile'
 import { styles } from './styles'
 
@@ -10,7 +10,7 @@ export function Home() {
   const [category, setCategory] = useState('')
 
   const handleCategorySelect = (categoryId: string) => {
-    categoryId === category  ? setCategory('') : setCategory(categoryId)
+    categoryId === category ? setCategory('') : setCategory(categoryId)
   }
   return (
     <View style={styles.container}>
@@ -18,12 +18,11 @@ export function Home() {
         <Profile />
         <ButtonAdd />
       </View>
-        <CategorySelect
-          categorySelected={category}
-          setCategory={handleCategorySelect}
-        />
-      <View>
-      </View>
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+      <View></View>
     </View>
   )
 }
