477446abc93b69a429cc343a633c4a74cdf96a43
Lazaro1 <lazarolimasilva2@gmail.com>
2021-12-24 11:11:35 -0300

    finish home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,45 @@
 import React, { useState } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
+import { Appointments } from '../../components/Appointment'
 import { ButtonAdd } from '../../components/ButtonAdd'
 import { CategorySelect } from '../../components/CategorySelect'
+import { ListDivider } from '../../components/ListDivider'
+import { ListHeader } from '../../components/ListHeader'
 
 import { Profile } from '../Profile'
 import { styles } from './styles'
 
 export function Home() {
   const [category, setCategory] = useState('')
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendarios',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06/ as 20:40h',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendarios',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06/ as 20:40h',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    }
+  ]
 
   const handleCategorySelect = (categoryId: string) => {
     categoryId === category ? setCategory('') : setCategory(categoryId)
@@ -22,7 +54,17 @@ export function Home() {
         categorySelected={category}
         setCategory={handleCategorySelect}
       />
-      <View></View>
+      <View style={styles.content}></View>
+      <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+
+      <FlatList
+        data={appointments}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Appointments data={item} />}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.matches}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   )
 }
